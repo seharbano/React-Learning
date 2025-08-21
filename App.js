7e6d2c77a9f128e05edee2e246cde2a61aa6d5af
seharbano <seharmlik670@gmail.com>
2025-08-21 16:36:24 +0500

    first three lectures

--- App.js
+++ App.js
@@ -1,3 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+const Title = () => {
+  return (
+    <div>
+      <h1 className="heading">React Learning smoothly</h1>
+    </div>
+  )
+}
+const SecondComponent = () => {
+  return (
+    <div className="bg-amber-100">
+      <Title/>
+      <h1 className="heading">Hi React 🚀</h1>
+    </div>
+  )
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<SecondComponent/>);
+
+
+
+
+
+
+
+
+
+
 /* 
 * <div id="parent">
 * <div id="child ">
@@ -9,20 +38,30 @@
 *
 */
 
-const parent = React.createElement(
-    'div',
-    { id: "parent" },
-    React.createElement(
-        'div',
-        { id: "child" },
-       [ React.createElement("h1", {}, "i am h1 tag!"),
-        React.createElement("h2", {}, "i am h2 tag!")]
-    )
-);
-console.log(parent);
-// const heading = React.createElement("h1", { id: "heading" }, "Hello World From React");
+// const parent = React.createElement(
+//     "div",
+//     { id: "parent",
+//         style: { backgroundColor: "black", padding: "20px" } 
+//      },
+//     React.createElement(
+//       "div",
+//       { id: "child",
+//         style: { backgroundColor: "black", padding: "20px" } 
+//        },
+//       [
+//         React.createElement("h1", { key: "h1",    style: { backgroundColor: "yellow",color:"red" ,  padding: "20px" }  }, "React Learning Start Now!"),
+//         React.createElement("h2", { key: "h2" }, "i am h2 tag!")
+//       ],
+//       [
+//         React.createElement("h1", { key: "h1",    style: { backgroundColor: "green",color:"white" ,  padding: "20px" }  }, "React Learning Start Now!"),
+//         React.createElement("h2", { key: "h2" }, "i am h2 tag!")
+//       ]
+//     )
+//   );
+// console.log(parent);
+// // const heading = React.createElement("h1", { id: "heading" }, "Hello World From React");
 
-// console.log(heading);//object
+// // console.log(heading);//object
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
--- README.md
+++ README.md
@@ -1,2 +1,31 @@
- # React Learning
+ # React Learning 
 
+
+# Parcel 
+_ Dev Build
+_ Local Server
+_ HMR = Hot Module Replacement
+_ Parcel Watching Algorithm - written in C++
+_ caching - faster build 
+_ Image Optimaization
+_ Minification 
+_ Building
+_ Compress 
+_ Consistent Hashing
+_ Code Splitting
+_ Differentail Bundling _ Support Older Browser
+_ Diagnostic
+_ Error Handling
+_ HTTPS
+_ Tree Shaking - removed unused code 
+_ Different dev and production bundles
+
+
+=> ############# ######## #######
+_ components two types
+_1 class components
+_2 functional components
+
+=>#### ##### #####
+_ component composition
+_ 
